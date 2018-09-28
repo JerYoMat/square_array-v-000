89f726739764da3db32696a2930d3f3c2c880c98
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 07:34:34 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
   # your code here
+  index_counter = 0
+  new_array = []
   array.each do |item|
-    
+    item = item * item
+    new_array[index_counter] = item 
+    index_counter += 1 
+  end 
 end
